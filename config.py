a54cb75c4098054ccc6bca808f1845bec363b8e6
jacobjj <jjj025@ucsd.edu>
2021-03-04 10:26:47 -0800

    config files with dimension values

---
+++ config.py
@@ -0,0 +1,9 @@
+# The configuration files for defining the size, and location of obstacles
+import numpy as np
+
+# Box obstacles
+xy = [np.r_[2,2], np.r_[2, 8], np.r_[5,5], np.r_[8, 2], np.r_[8, 8]] # Box positions
+box_length, box_width  = 1., 1.
+
+# Circle Obstacles
+cir_radius = 0.5
