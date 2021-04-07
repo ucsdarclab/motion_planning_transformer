707da06faad4a771dfbebec7d72a4771733b7436
jacobjj <jjj025@ucsd.edu>
2021-04-06 21:52:59 -0700

    script to run multiple eval

---
+++ docker/run_eval.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# A script to run the eval lib multiple times
+
+for CID in {1..5}
+do
+    docker run \
+    -d \
+    --rm \
+    --gpus all \
+    -v /home/jacoblab/global_planner:/workspace \
+    -v /home/jacoblab/global_planner_data:/root/data \
+    pytorch/pytorch:1.7.1-cuda11.0-cudnn8-jupyter-ompl\
+    python3 eval_model.py $CID
+done
