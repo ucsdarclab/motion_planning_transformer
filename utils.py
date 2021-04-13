ea04b2fbdec9a4b080d1d231f6209a0ff9c8df6f
jacobjj <jjj025@ucsd.edu>
2021-04-13 09:20:54 -0700

    added general geom2pix function

--- utils.py
+++ utils.py
@@ -3,6 +3,7 @@
 import skimage.io
 import io
 
+import numpy as np
 
 def png_decoder(key, value):
     '''
@@ -28,3 +29,24 @@ def cls_decoder(key, value):
     assert isinstance(value, bytes)
     return int(value)
 
+def geom2pix(pos, res=0.05, size=(480, 480)):
+    """
+    Convert geometrical position to pixel co-ordinates. The origin 
+    is assumed to be at [image_size[0]-1, 0].
+    :param pos: The (x,y) geometric co-ordinates.
+    :param res: The distance represented by each pixel.
+    :param size: The size of the map image
+    :returns (int, int): The associated pixel co-ordinates.
+    NOTE: The Pixel co-ordinates are represented as follows:
+    (0,0)------ X ----------- |
+    |                         |  
+    |                         |  
+    |                         |  
+    |                         |  
+    Y                         |
+    |                         |
+    |                         |  
+    |                         |  
+    ---------------------------  
+    """
+    return (np.int(np.floor(pos[0]/res)), np.int(size[0]-1-np.floor(pos[1]/res)))
