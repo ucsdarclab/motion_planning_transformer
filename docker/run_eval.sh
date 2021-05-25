b49e09a9953e68dff5991d2b8e1fa0fcf3b802e0
jacobjj <jjj025@ucsd.edu>
2021-05-25 01:27:12 -0700

    Run evaluation

--- docker/run_eval.sh
+++ docker/run_eval.sh
@@ -11,9 +11,9 @@ do
     -v /home/jacoblab/global_planner_data:/root/data \
     pytorch/pytorch:1.7.1-cuda11.0-cudnn8-jupyter-ompl\
     python3 eval_model.py \
-    --modelFolder=/root/data/model29 \
+    --modelFolder=/root/data/model38 \
     --start=$((900+CID*20)) \
-    --valDataFolder=/root/data/maze3/val \
-    --epoch=24\
+    --valDataFolder=/root/data/maze4/val \
+    --epoch=69\
     --numPaths=25
 done
