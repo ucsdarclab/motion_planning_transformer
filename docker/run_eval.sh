3d7d77d52b77e43003c05bd2e6ed14fb6fafdc18
jacobjj <jjj025@ucsd.edu>
2021-05-25 23:51:52 -0700

    update scipt

--- docker/run_eval.sh
+++ docker/run_eval.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # A script to run the eval lib multiple times
-
+SAMPLES=20
 for CID in {0..4}
 do
     docker run \
@@ -12,7 +12,9 @@ do
     pytorch/pytorch:1.7.1-cuda11.0-cudnn8-jupyter-ompl\
     python3 eval_model.py \
     --modelFolder=/root/data/model38 \
-    --start=$((900+CID*20)) \
+    --plannerType=rrtstar \
+    --start=$((900+CID*SAMPLES)) \
+    --samples=$SAMPLES\
     --valDataFolder=/root/data/maze4/val \
     --epoch=69\
     --numPaths=25
