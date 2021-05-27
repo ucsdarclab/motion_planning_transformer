3b5ab451e9a98df84e7d0a7c37e6085a86f52c0d
jacobjj <jjj025@ucsd.edu>
2021-05-26 19:41:35 -0700

    generate maps of any size

--- generateMaps.py
+++ generateMaps.py
@@ -6,8 +6,8 @@ import matplotlib.pyplot as plt
 import matplotlib.transforms as mattrans
 
 def generate_random_maps(
-    length, 
-    breadth=None, 
+    width, 
+    height=None, 
     dist_resl=0.05,
     num_circle=70,
     num_box=30,
@@ -15,41 +15,42 @@ def generate_random_maps(
     fileName=None
     ):
     '''
-    Randomly generate maps with the given length and breadth, resolution and
+    Randomly generate maps with the given width and height, resolution and
     obstacles.
-    :param length: The length of the map in meters.
-    :param breath: TODO.... The breadth of the map.
+    :param width: The width of the map in meters.
+    :param breath: TODO.... The height of the map.
     :param dist_resl: distance(in meters) per pixels
     :param num_circle: Number of circular objects.
     :param num_box: Number of square objects.
     :param seed: The random seed value for random number generator.
     :param fileName: File name to save the map, if None the map is saved as 'map_temp.png'
     '''
-    assert breadth is None, "The current function only set's up square maps!!"
-    box_length, box_width = 1.5, 1.5
+    if height is None:
+        height = width
+    box_width, box_width = 1.5, 1.5
     cir_radius = 0.75
 
     np.random.seed(seed)
 
-    xy_circle = np.random.rand(num_circle, 2)*(length-1)+0.5
-
-    xy_box = np.random.rand(num_box, 2)*(length-1)+0.5
+    xy_circle = np.c_[np.random.rand(num_circle)*(width-1)+0.5,  np.random.rand(num_circle)*(height-1)+0.5]
+    xy_box = np.c_[np.random.rand(num_box)*(width-1)+0.5, np.random.rand(num_box)*(height-1)+0.5]
     xy_theta = np.random.rand(num_box)*np.pi*2
 
 
-    size = length*100/2.54 #In inches
+    sizeW = width*100/2.54 #In inches
+    sizeH = height*100/2.54 #In inches
     dpi = 0.0254/dist_resl # dots per inches, convert the resolution to inches
 
-    fig, ax = plt.subplots(figsize=(size, size), dpi=dpi)
+    fig, ax = plt.subplots(figsize=(sizeW, sizeH), dpi=dpi)
     # NOTE: This is important to ensure that the loaded map, matches the set
     # distance resolution.
     plt.subplots_adjust(0, 0, 1, 1) # Removes all spacings on either sides of the map
 
     # Initialize the position of obstacles
-    dimensions = [box_length, box_width]
+    dimensions = [box_width, box_width]
     rectangle_corner = np.r_[(-dimensions[0]/2, -dimensions[1]/2)]
     
-    plt_boundary = plt.Rectangle((0,0), length, length, color='k', fill=None, lw=2, alpha=1,)
+    plt_boundary = plt.Rectangle((0,0), width, height, color='k', fill=None, lw=2, alpha=1,)
     ax.add_patch(plt_boundary)
         
     for xy_i in  xy_circle:
@@ -65,8 +66,8 @@ def generate_random_maps(
     ax.set_xticks([])
     ax.set_yticks([])
 
-    ax.set_xlim([0, length])
-    ax.set_ylim([0, length])
+    ax.set_xlim([0, width])
+    ax.set_ylim([0, height])
     
     if fileName is None:
         fig.savefig('map_temp.png', pad_inches=0.0, bbox_inches='tight')
