c1848e2700fbff6d59a02d58db60cdf577ef59c3
jacobjj <jjj025@ucsd.edu>
2021-09-12 19:10:43 -0700

    reduce size of unet

--- unet/Models.py
+++ unet/Models.py
@@ -13,17 +13,29 @@ class UNet(nn.Module):
         self.n_classes = n_classes
         self.bilinear = bilinear
 
-        self.inc = DoubleConv(n_channels, 64)
-        self.down1 = Down(64, 128)
-        self.down2 = Down(128, 256)
-        self.down3 = Down(256, 512)
+        # self.inc = DoubleConv(n_channels, 64)
+        # self.down1 = Down(64, 128)
+        # self.down2 = Down(128, 256)
+        # self.down3 = Down(256, 512)
+        # factor = 2 if bilinear else 1
+        # self.down4 = Down(512, 1024 // factor)
+        # self.up1 = Up(1024, 512 // factor, bilinear)
+        # self.up2 = Up(512, 256 // factor, bilinear)
+        # self.up3 = Up(256, 128 // factor, bilinear)
+        # self.up4 = Up(128, 64, bilinear)
+        # self.outc = OutConv(64, n_classes)
+
+        self.inc = DoubleConv(n_channels, 16)
+        self.down1 = Down(16, 32)
+        self.down2 = Down(32, 64)
+        self.down3 = Down(64, 128)
         factor = 2 if bilinear else 1
-        self.down4 = Down(512, 1024 // factor)
-        self.up1 = Up(1024, 512 // factor, bilinear)
-        self.up2 = Up(512, 256 // factor, bilinear)
-        self.up3 = Up(256, 128 // factor, bilinear)
-        self.up4 = Up(128, 64, bilinear)
-        self.outc = OutConv(64, n_classes)
+        self.down4 = Down(128, 256 // factor)
+        self.up1 = Up(256, 128 // factor, bilinear)
+        self.up2 = Up(128, 64 // factor, bilinear)
+        self.up3 = Up(64, 32 // factor, bilinear)
+        self.up4 = Up(32, 16, bilinear)
+        self.outc = OutConv(16, n_classes)
 
     def forward(self, x):
         x1 = self.inc(x)
