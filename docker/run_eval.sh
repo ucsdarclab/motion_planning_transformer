59c7ab266c941e9835fc023584a0782dafe77d50
jacobjj <jjj025@ucsd.edu>
2021-09-14 09:09:44 -0700

    added new parameters

--- docker/run_eval.sh
+++ docker/run_eval.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # A script to run the eval lib multiple times
-SAMPLES=20
-for CID in {0..4}
+SAMPLES=125
+for CID in {0..20}
 do
     docker run \
     -d \
@@ -11,11 +11,12 @@ do
     -v /home/jacoblab/global_planner_data:/root/data \
     pytorch/pytorch:1.7.1-cuda11.0-cudnn8-jupyter-ompl\
     python3 eval_model.py \
-    --modelFolder=/root/data/model38 \
+    --segmentType=unet \
+    --modelFolder=/root/data/unet/model0 \
     --plannerType=rrtstar \
-    --start=$((900+CID*SAMPLES)) \
+    --start=$((CID*SAMPLES)) \
     --samples=$SAMPLES\
-    --valDataFolder=/root/data/maze4/val \
+    --valDataFolder=/root/data/forest/val \
     --epoch=69\
-    --numPaths=25
+    --numPaths=1
 done
