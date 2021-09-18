6a90efdfeff4baa0f50b96d2549d5142e13aa8ac
jacobjj <jjj025@ucsd.edu>
2021-09-17 20:45:41 -0700

    script for running global planer

---
+++ docker/run_eval_mpnet.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# A script to run the eval lib multiple times
+SAMPLES=125
+for CID in {0..19}
+do
+    docker run \
+    -d \
+    --rm \
+    --gpus all \
+    -v /home/jacoblab/global_planner:/workspace \
+    -v /home/jacoblab/global_planner_data:/root/data \
+    pytorch/pytorch:1.7.1-cuda11.0-cudnn8-jupyter-ompl\
+    python3 eval_model_mpnet.py \
+    --modelFolder=/root/data/mpnet/model0 \
+    --start=$((CID*SAMPLES)) \
+    --samples=$SAMPLES\
+    --valDataFolder=/root/data/forest/val \
+    --epoch=69\
+    --numPaths=1
+done
