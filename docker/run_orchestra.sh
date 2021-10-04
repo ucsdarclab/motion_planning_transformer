1ce1ae3aab9f148f3963918d5bd2550394981adf
jacobjj <jjj025@ucsd.edu>
2021-10-03 17:27:05 -0700

    update map collection code

--- docker/run_orchestra.sh
+++ docker/run_orchestra.sh
@@ -1,23 +1,20 @@
 #!/bin/bash
 # A script to run ompl-docker
-SAMPLES=20
-for CID in {0..14..1}
+SAMPLES=50
+for CID in {0..19}
 do
-	docker run -d \
+	docker run -d\
 	    --rm \
-	    --name=data_$1_$CID \
 	    --shm-size="2g"\
-	    -e DISPLAY=$DISPLAY \
-	    -e QT_X11_NO_MITSHM=1 \
-	    -v $XAUTH:/root/.Xauthority \
-	    -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
-	    -v ~/global_planner:/root/global_planner \
+	    -v ~/global_planner:/workspace \
 	    -v ~/global_planner_data:/root/data \
-	    ompl-global \
+	    mpt \
 	    python3 rrt_star_map.py \
 			--start=$((CID*SAMPLES)) \
-			--samples=$SAMPLES \
+			--numEnv=$SAMPLES \
 			--envType=maze \
-			--fileDir=/root/data/maze2/train \
-			--numPaths=25 
+			--fileDir=/root/data/maze4/val780 \
+			--numPaths=1\
+			--height=710 \
+			--width=710		
 done
